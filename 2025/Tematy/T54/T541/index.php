4e714139965763587591a0aec35f4c432a56b24c
2p02 <jakub.pieniezny@onet.pl>
2025-05-12 15:52:13 +0200

    Zadanie T541 rozpoczęte

---
+++ 2025/Tematy/T54/T541/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function wbz1($x)
+{
+ if ($x > 0) return "$x";
+ else return $x * -1;
+} ////////////////////////////////////////////
+
+
+
+
+
+
+
+echo wbz1(-12)."<br>";
+echo wbz1(32);
+
+
