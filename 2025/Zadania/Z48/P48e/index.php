24597b2a8e706d543afc972b94d7d7b212be3a35
JakubPien <144221606+JakubPien@users.noreply.github.com>
2025-05-23 17:12:22 +0200

    p48e
    poprawione do sprawdzenia

--- 2025/Zadania/Z48/P48e/index.php
+++ 2025/Zadania/Z48/P48e/index.php
@@ -22,9 +22,19 @@ pyta użytkownika o długość losowego słowa oraz ilość słów,
 wyświetla wzorcową 10 znakową tablicę,
 wyświetla podaną ilość słów składających się z podanej ilości znaków wygenerowanych ze znaków zapisanych w 10-elementowej tablicy stworzonej na początku programu.
     </pre>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <input type="text">
-        <input type="submit" value="Wyślij">
+     <form method="post">
+        <fieldset>
+            <legend>Podaj 10 znaków:</legend>
+            <?php
+            for ($i = 0; $i < 10; $i++) {
+                echo 'Znak ' . ($i + 1) . ': <input type="text" name="znak[]" maxlength="1" required><br>';
+            }
+            ?>
+        </fieldset>
+        <br>
+        Długość słowa: <input type="number" name="dlugosc" min="1" required><br><br>
+        Ilość słów: <input type="number" name="ilosc" min="1" required><br><br>
+        <input type="submit" value="Generuj słowa">
     </form>
 </section>
 
@@ -33,40 +43,26 @@ wyświetla podaną ilość słów składających się z podanej ilości znaków 
 
 
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $tablicaZnakow = $_POST["znak"];
+        $dlugoscSlowa = (int)$_POST["dlugosc"];
+        $iloscSlow = (int)$_POST["ilosc"];
 
-    $tablicaZnakow = [];
-    echo "Podaj 10 znaków:\n";
-    for ($i = 0; $i < 10; $i++) {
-        echo "Znak " . ($i + 1) . ": ";
-        $znak = trim(fgets(STDIN));
-    while (strlen($znak) !== 1) {
-        echo "Podaj dokładnie jeden znak!\nZnak " . ($i + 1) . ": ";
-        $znak = trim(fgets(STDIN));
-    }
-    $tablicaZnakow[] = $znak;
-    }
+        if (count($tablicaZnakow) == 10 && $dlugoscSlowa > 0 && $iloscSlow > 0) {
+            echo "<h2>Wzorcowa tablica znaków:</h2>";
+            echo implode(" ", $tablicaZnakow) . "<br><br>";
 
-    echo "Podaj długość jednego słowa: ";
-    $dlugoscSlowa = (int)trim(fgets(STDIN));
-
-    echo "Podaj ilość słów do wygenerowania: ";
-    $iloscSlow = (int)trim(fgets(STDIN));
-
-    echo "\nWzorcowa tablica znaków:\n";
-    echo implode(" ", $tablicaZnakow) . "\n";
-
-    echo "\nWygenerowane słowa:\n";
-    for ($i = 0; $i < $iloscSlow; $i++) {
-       $slowo = "";
-       for ($j = 0; $j < $dlugoscSlowa; $j++) {
-          $losowyIndex = rand(0, 9);
-        $slowo .= $tablicaZnakow[$losowyIndex];
+            echo "<h2>Wygenerowane słowa:</h2>";
+            for ($i = 0; $i < $iloscSlow; $i++) {
+                $slowo = "";
+                for ($j = 0; $j < $dlugoscSlowa; $j++) {
+                    $index = rand(0, 9);
+                    $slowo .= $tablicaZnakow[$index];
+                }        
+                echo $slowo . "<br>";
+            }
+        } else {
+            echo "<p style='color:red;'>Upewnij się, że podałeś 10 znaków oraz poprawne liczby.</p>";
         }
-     echo $slowo . "\n";
-    }
-
-    }
     ?>
 </section>
 
