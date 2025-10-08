5e853a39ec833f374c9fd4cd587afe271eb7544e
3p02 <jakub.pieniezny@onet.pl>
2025-10-08 12:30:40 +0200

    Zadanie T60 a w trakcie

--- 2026/Tematy/T60a/index.php
+++ 2026/Tematy/T60a/index.php
@@ -24,48 +24,96 @@
         Trzeci przycisk wyświetla dane z tabeli pracownicy w postaci tabelarycznej.
     </p>
 
-    <input type="submit" id="stworz" value="Stwórz tabelę">
-    <input type="submit" id="zaladuj" value="Załaduj tabelę">
-    <input type="submit" id="wyswietl" value="Wyświetl zawartość">
-</section>
+    <form method="post">
+        <input type="submit" name="stworz" id="stworz" value="Stwórz tabelę">
+        <input type="submit" name="zaladuj" id="zaladuj" value="Załaduj tabelę">
+        <input type="submit" name="wyswietl" id="wyswietl" value="Wyświetl zawartość">
+        <input type="submit" name="usun" id="usun" value="Usuń tabelę"
+    </form>
 
-<section>
-
-<?php
-if (isset($_POST['stworz'])) {
+    <?php
     $db = mysqli_connect("localhost", "root", "", "3p_2_baza_pracownikow");
+    if (!$db) die("Błąd połączenia: " . mysqli_connect_error());
 
-    $create = "CREATE TABLE IF NOT EXISTS pracownicy";
+    mysqli_set_charset($db, "utf8mb4");
 
-    $wynik = mysqli_query($db, $create);
+    header("Content-Type: text/html; charset=utf-8");
 
-//CREATE TABLE IF NOT EXISTS pracownicy ( id INT PRIMARY KEY, nazwisko VARCHAR(50), imie VARCHAR(50), stanowisko VARCHAR(50), dzial VARCHAR(50), sekcja VARCHAR(20) )
-}
-?>
+    if (isset($_POST['stworz'])) {
+        $create = "CREATE TABLE IF NOT EXISTS pracownicy (
+        id INT PRIMARY KEY,
+        nazwisko VARCHAR(50),
+        imie VARCHAR(50),
+        stanowisko VARCHAR(50),
+        dzial VARCHAR(50),
+        sekcja VARCHAR(50)
+    )";
 
-<?php
-if (isset($_POST['zaladuj'])) {
-    $db = mysqli_connect("localhost", "root", "", "3p_2_baza_pracownikow");
-
-    $zal = "INSERT INTO pracownicy VALUES ";
-
-    $wynik = mysqli_query($db, $zal);
-}
-?>
-
-<?php
-if (isset($_POST['wyswietl'])) {
-    $db = mysqli_connect("localhost", "root", "", "3p_2_baza_pracownikow");
-    $wys = "SELECT * FROM pracownicy";
-
-    $wynik = mysqli_query($db, $wys);
-    echo "<tr>   <td>ID</td> <td>Nazwisko</td> <td>Imię</td><td>Stanowisko</td> <td>Dział</td> <td>Sekcja</td>    </tr>";
-    while ($el = mysqli_fetch_row($wynik)) {
-        echo "<tr>" . "<td>" . $el[0] . "</td><td>" . $el[1] . "</td><td>" . $el[2] . "</td><td>" . $el[3] . "</td><td>" . $el[4] . "</td><td>" . $el[5] . "</td></tr>";
+        if (mysqli_query($db, $create)) echo "Tabela została stworzona.<br>";
+        else echo "Błąd: " . mysqli_error($db);
     }
-}
-?>
 
+    if (isset($_POST['zaladuj'])) {
+        mysqli_query($db, "TRUNCATE TABLE pracownicy");
+
+        $plik = fopen("pracownicy.csv", "r");
+        if (!$plik) {
+            die("Nie można otworzyć pliku dane.csv");
+        }
+
+        $header = fgetcsv($plik, 1000, ";");
+
+        while (($data = fgetcsv($plik, 1000, ";")) !== false) {
+            $id = $data[0];
+            $nazwisko = $data[1];
+            $imie = $data[2];
+            $stanowisko = $data[3];
+            $dzial = $data[4];
+            $sekcja = $data[5];
+
+            $sql = "INSERT INTO pracownicy (id, nazwisko, imie, stanowisko, dzial, sekcja)
+                VALUES ('$id','$nazwisko','$imie','$stanowisko','$dzial','$sekcja')
+                ON DUPLICATE KEY UPDATE
+                    nazwisko='$nazwisko',
+                    imie='$imie',
+                    stanowisko='$stanowisko',
+                    dzial='$dzial',
+                    sekcja='$sekcja'";
+
+            if (!mysqli_query($db, $sql)) {
+                echo "Błąd wstawiania ID=$id: " . mysqli_error($db) . "<br>";
+            }
+        }
+
+        fclose($plik);
+        echo "Dane z CSV zostały wczytane.<br>";
+    }
+
+    if (isset($_POST['wyswietl'])) {
+        $result = mysqli_query($db, "SELECT * FROM pracownicy");
+
+        echo "<table border='1' cellpadding='5'>
+            <tr>
+                <th>ID</th><th>Nazwisko</th><th>Imię</th><th>Stanowisko</th><th>Dział</th><th>Sekcja</th>
+            </tr>";
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>
+                <td>{$row['id']}</td>
+                <td>{$row['nazwisko']}</td>
+                <td>{$row['imie']}</td>
+                <td>{$row['stanowisko']}</td>
+                <td>{$row['dzial']}</td>
+                <td>{$row['sekcja']}</td>
+              </tr>";
+        }
+
+        echo "</table>";
+    }
+
+    mysqli_close($db);
+
+    ?>
 </section>
 
 </body>
