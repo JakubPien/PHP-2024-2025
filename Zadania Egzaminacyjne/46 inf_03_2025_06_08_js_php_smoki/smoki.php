7cb5775b1f73c0a0a0d0383295ab001a330c2146
3p02 <jakub.pieniezny@onet.pl>
2026-01-07 12:28:45 +0100

    Zadanie egzaminacjyjne smoki w trakcie

---
+++ Zadania Egzaminacyjne/46 inf_03_2025_06_08_js_php_smoki/smoki.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Smoki</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+
+<header>
+    Blok nagłówkowy
+    <h2>Poznaj smoki</h2>
+</header>
+
+<main id="1">
+    Blok nawigacyjny
+
+    <nav id="1">
+        <p>Baza</p>
+    </nav>
+    <nav id="2">
+        <p>Opisy</p>
+    </nav>
+    <nav id="3"></nav>
+</main>
+
+<main id="2">
+    Blok główny
+
+    <section id="1"></section>
+    <section id="2"></section>
+    <section id="3"></section>
+</main>
+
+
+
+
+<footer>
+    Blok stopki
+</footer>
+
+</body>
+</html>
+
+<?php
+
+?>
